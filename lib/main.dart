17699dffcd52bafbaff70f0f08690c70f11ef77c
miravicson <victorughonu@gmail.com>
2023-06-29 10:24:15 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
+import 'package:fooderlich/home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
